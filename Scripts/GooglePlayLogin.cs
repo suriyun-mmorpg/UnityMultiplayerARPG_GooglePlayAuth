517dce652623cdd0aad9210b6c92284d6c183a6b
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-07-08 18:08:14 +0700

    Finished Google play login integration

--- Scripts/GooglePlayLogin.cs
+++ Scripts/GooglePlayLogin.cs
@@ -10,39 +10,37 @@ namespace MultiplayerARPG.MMO
 {
     public class GooglePlayLogin : MonoBehaviour
     {
-        private static readonly PlayGamesClientConfiguration ClientConfiguration = new PlayGamesClientConfiguration.Builder()
-            // requests the email address of the player be available.
-            // Will bring up a prompt for consent.
-            .RequestEmail()
-            // requests a server auth code be generated so it can be passed to an
-            //  associated back end server application and exchanged for an OAuth token.
-            .RequestServerAuthCode(false)
-            // requests an ID token be generated.  This OAuth token can be used to
-            //  identify the player to other services such as Firebase.
-            .RequestIdToken()
-            .Build();
-
         public bool debugLogEnabled;
         public UnityEvent onLoginSuccess;
         public UnityEvent onLoginFail;
 
+        private void Awake()
+        {
+            var builder = new PlayGamesClientConfiguration.Builder()
+                // requests the email address of the player be available.
+                // Will bring up a prompt for consent.
+                .RequestEmail()
+                // requests a server auth code be generated so it can be passed to an
+                //  associated back end server application and exchanged for an OAuth token.
+                .RequestServerAuthCode(false)
+                // requests an ID token be generated.  This OAuth token can be used to
+                //  identify the player to other services such as Firebase.
+                .RequestIdToken();
+            var config = builder.Build();
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = debugLogEnabled;
+        }
+
         public void OnClickGooglePlayLogin()
         {
-            PlayGamesPlatform.InitializeInstance(ClientConfiguration);
-            PlayGamesPlatform.DebugLogEnabled = debugLogEnabled;
-            PlayGamesPlatform.Activate();
             PlayGamesPlatform.Instance.SignOut();
             PlayGamesPlatform.Instance.Authenticate((success, message) => {
                 if (success)
                 {
                     // When google play login success, send login request to server
-                    var id = PlayGamesPlatform.Instance.GetUserId();
-                    var userName = PlayGamesPlatform.Instance.GetUserDisplayName();
                     var email = PlayGamesPlatform.Instance.GetUserEmail();
                     var idToken = PlayGamesPlatform.Instance.GetIdToken();
-                    PlayGamesPlatform.Instance.GetServerAuthCode();
-                    UISceneGlobal.Singleton.ShowMessageDialog("Login", " id: " + id + " userName: " + userName + " email: " + email + " idToken:" + idToken);
-                    //RequestGooglePlayLogin(email, idToken);
+                    RequestGooglePlayLogin(email, idToken);
                 }
                 else
                 {
