15b582ed22fb18a13bf8145c0ecc080a2c35b9c3
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-07-08 07:34:59 +0700

    WIP Google play login

--- Scripts/GooglePlayLogin.cs
+++ Scripts/GooglePlayLogin.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.SocialPlatforms;
 using LiteNetLibManager;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
@@ -32,20 +31,68 @@ namespace MultiplayerARPG.MMO
             PlayGamesPlatform.InitializeInstance(ClientConfiguration);
             PlayGamesPlatform.DebugLogEnabled = debugLogEnabled;
             PlayGamesPlatform.Activate();
-            Social.localUser.Authenticate((bool success) => {
-                // handle success or failure
+            PlayGamesPlatform.Instance.SignOut();
+            PlayGamesPlatform.Instance.Authenticate((success, message) => {
                 if (success)
                 {
                     // When google play login success, send login request to server
-                    var userName = Social.localUser.userName;
-                    var idToken = ((PlayGamesLocalUser)Social.localUser).GetIdToken();
+                    var id = PlayGamesPlatform.Instance.GetUserId();
+                    var userName = PlayGamesPlatform.Instance.GetUserDisplayName();
+                    var email = PlayGamesPlatform.Instance.GetUserEmail();
+                    var idToken = PlayGamesPlatform.Instance.GetIdToken();
+                    PlayGamesPlatform.Instance.GetServerAuthCode();
+                    UISceneGlobal.Singleton.ShowMessageDialog("Login", " id: " + id + " userName: " + userName + " email: " + email + " idToken:" + idToken);
+                    //RequestGooglePlayLogin(email, idToken);
                 }
                 else
                 {
                     // Show error message
-                    UISceneGlobal.Singleton.ShowMessageDialog("Cannot Login", "Cannot Login with Google Play");
+                    UISceneGlobal.Singleton.ShowMessageDialog("Cannot Login", "Cannot Login with Google Play: " + message);
                 }
             });
+        }
+
+        private void RequestGooglePlayLogin(string email, string idToken)
+        {
+            var uiSceneGlobal = UISceneGlobal.Singleton;
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(idToken))
+            {
+                uiSceneGlobal.ShowMessageDialog("Cannot login", "User Email or ID token is empty");
+                return;
+            }
+            MMOClientInstance.Singleton.RequestGooglePlayLogin(email, idToken, OnLogin);
+        }
+
+        public void OnLogin(AckResponseCode responseCode, BaseAckMessage message)
+        {
+            var castedMessage = (ResponseUserLoginMessage)message;
+            switch (responseCode)
+            {
+                case AckResponseCode.Error:
+                    var errorMessage = string.Empty;
+                    switch (castedMessage.error)
+                    {
+                        case ResponseUserLoginMessage.Error.AlreadyLogin:
+                            errorMessage = "User already logged in";
+                            break;
+                        case ResponseUserLoginMessage.Error.InvalidUsernameOrPassword:
+                            errorMessage = "Invalid username or password";
+                            break;
+                    }
+                    UISceneGlobal.Singleton.ShowMessageDialog("Cannot Login", errorMessage);
+                    if (onLoginFail != null)
+                        onLoginFail.Invoke();
+                    break;
+                case AckResponseCode.Timeout:
+                    UISceneGlobal.Singleton.ShowMessageDialog("Cannot Login", "Connection timeout");
+                    if (onLoginFail != null)
+                        onLoginFail.Invoke();
+                    break;
+                default:
+                    if (onLoginSuccess != null)
+                        onLoginSuccess.Invoke();
+                    break;
+            }
         }
     }
 }
