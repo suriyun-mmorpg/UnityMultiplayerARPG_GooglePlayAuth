9b7b308c9b38ff248d14294d9878225aa13f8c3f
Insthync <ittipon.bay@gmail.com>
2020-08-16 07:46:26 +0700

    Change all mysql database query functions to be async and also remove rows reader it's not required and for better performance

--- Scripts/MySQLDatabase_GooglePlayLogin.cs
+++ Scripts/MySQLDatabase_GooglePlayLogin.cs
@@ -1,36 +1,31 @@
 ﻿using MySqlConnector;
+using System.Threading.Tasks;
 
 namespace MultiplayerARPG.MMO
 {
     public partial class MySQLDatabase
     {
-        public override string GooglePlayLogin(string gpgId, string email)
+        public override async Task<string> GooglePlayLogin(string gpgId, string email)
         {
             string id = string.Empty;
-            MySQLRowsReader reader = ExecuteReader("SELECT id FROM userlogin WHERE username=@username AND password=@password AND authType=@authType LIMIT 1",
+            await ExecuteReader((reader) =>
+            {
+                if (reader.Read())
+                    id = reader.GetString("id");
+            }, "SELECT id FROM userlogin WHERE username=@username AND password=@password AND authType=@authType LIMIT 1",
                 new MySqlParameter("@username", "g_" + gpgId),
                 new MySqlParameter("@password", GenericUtils.GetMD5(gpgId)),
                 new MySqlParameter("@authType", AUTH_TYPE_GOOGLE_PLAY));
 
-            if (reader.Read())
-                id = reader.GetString("id");
-            else
+            if (string.IsNullOrEmpty(id))
             {
-                ExecuteNonQuery("INSERT INTO userlogin (id, username, password, email, authType) VALUES (@id, @username, @password, @email, @authType)",
-                    new MySqlParameter("@id", GenericUtils.GetUniqueId()),
+                id = GenericUtils.GetUniqueId();
+                await ExecuteNonQuery("INSERT INTO userlogin (id, username, password, email, authType) VALUES (@id, @username, @password, @email, @authType)",
+                    new MySqlParameter("@id", id),
                     new MySqlParameter("@username", "g_" + gpgId),
                     new MySqlParameter("@password", GenericUtils.GetMD5(gpgId)),
                     new MySqlParameter("@email", email),
                     new MySqlParameter("@authType", AUTH_TYPE_GOOGLE_PLAY));
-
-                // Read last entry
-                reader = ExecuteReader("SELECT id FROM userlogin WHERE username=@username AND password=@password AND authType=@authType LIMIT 1",
-                    new MySqlParameter("@username", "g_" + gpgId),
-                    new MySqlParameter("@password", GenericUtils.GetMD5(gpgId)),
-                    new MySqlParameter("@authType", AUTH_TYPE_GOOGLE_PLAY));
-
-                if (reader.Read())
-                    id = reader.GetString("id");
             }
             return id;
         }
