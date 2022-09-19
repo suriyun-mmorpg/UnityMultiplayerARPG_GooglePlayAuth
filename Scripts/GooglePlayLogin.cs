a95819e6c0166fb1e0bc59259dd55321c4b7529a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-09-19 16:23:07 +0700

    Update plugins

--- Scripts/GooglePlayLogin.cs
+++ Scripts/GooglePlayLogin.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 using LiteNetLibManager;
+using System.Text.RegularExpressions;
+using Cysharp.Threading.Tasks;
 #if UNITY_ANDROID
-using GooglePlayGames;
-using GooglePlayGames.BasicApi;
+using Google;
+using Google.Impl;
 #endif
 
 namespace MultiplayerARPG.MMO
@@ -13,47 +15,50 @@ namespace MultiplayerARPG.MMO
         public bool debugLogEnabled;
         public UnityEvent onLoginSuccess;
         public UnityEvent onLoginFail;
+        public string webClientId;
 
         private void Start()
         {
 #if UNITY_ANDROID
-            PlayGamesPlatform.DebugLogEnabled = debugLogEnabled;
+            GoogleSignIn.Configuration = new GoogleSignInConfiguration();
 #endif
         }
 
-        public void OnClickGooglePlayLogin()
+        public async void OnClickGooglePlayLogin()
         {
 #if UNITY_ANDROID
-            PlayGamesPlatform.Instance.Authenticate((status) =>
+            GoogleSignIn.Configuration.WebClientId = webClientId;
+            GoogleSignIn.Configuration.RequestEmail = true;
+            GoogleSignIn.Configuration.RequestAuthCode = true;
+            GoogleSignIn.Configuration.RequestIdToken = true;
+
+            try
             {
-                switch (status)
-                {
-                    case SignInStatus.InternalError:
-                        UISceneGlobal.Singleton.ShowMessageDialog("Cannot Login", "Internal Errror, cannot Login with Google Play.");
-                        break;
-                    case SignInStatus.Canceled:
-                        UISceneGlobal.Singleton.ShowMessageDialog("Cannot Login", "Login with Google Play was cancelled.");
-                        break;
-                    default:
-                        // When google play login success, send login request to server
-                        PlayGamesPlatform.Instance.RequestServerSideAccess(false, (idToken) =>
-                        {
-                            RequestGooglePlayLogin(idToken);
-                        });
-                        break;
-                }
-            });
+                GoogleSignInUser result = await GoogleSignIn.DefaultInstance.SignIn();
+                RequestGooglePlayLogin(result.IdToken);
+            }
+            catch (GoogleSignIn.SignInException ex)
+            {
+                string error = ex.Status.ToString();
+                Logging.LogError("Error occuring when signin with Google: " + Regex.Replace(error, "(\\B[A-Z])", " $1"));
+                UISceneGlobal.Singleton.ShowMessageDialog("Cannot Login", "Error, cannot Login with Google Play.");
+            }
+            catch (System.Exception ex)
+            {
+                Logging.LogError("Error occuring when signin with Google: " + ex.StackTrace);
+                UISceneGlobal.Singleton.ShowMessageDialog("Cannot Login", "Error, cannot Login with Google Play.");
+            }
 #else
             Debug.Log("Only Android can login with Google Play");
+            await UniTask.Yield();
 #endif
         }
 
         private void RequestGooglePlayLogin(string idToken)
         {
-            UISceneGlobal uiSceneGlobal = UISceneGlobal.Singleton;
             if (string.IsNullOrEmpty(idToken))
             {
-                uiSceneGlobal.ShowMessageDialog("Cannot login", "ID token is empty");
+                UISceneGlobal.Singleton.ShowMessageDialog("Cannot login", "ID token is empty");
                 return;
             }
             MMOClientInstance.Singleton.RequestGooglePlayLogin(idToken, OnLogin);
